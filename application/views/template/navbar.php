2b9b7e3308e51daab8cf06bd4d0f8b75b831e0f6
Agung Satriawan <45866024+agungSatriawan@users.noreply.github.com>
2023-12-03 02:10:03 +0700

    perbaikan path

--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -1,14 +1,14 @@
   <!-- Nav section start -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
       <div class="container-fluid">
-          <a class="navbar-brand" href="/sarikusuma">SARI KUSUMA 99</a>
+          <a class="navbar-brand" href="/sarikusuma"><?= $title ?></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                      <a class="nav-link active" aria-current="page" href="/sarikusuma">Home</a>
+                      <a class="nav-link active" aria-current="page" href="<?= base_url() ?>">Home</a>
                   </li>
                   <li class="nav-item">
                       <a class="nav-link" href="#about">About</a>
@@ -31,7 +31,7 @@
               <div class="d-flex">
                   <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                       <li class="nav-item">
-                          <a href="/sarikusuma/login" class="nav-link active">Login</a>
+                          <a href="<?= base_url('login') ?>" class="nav-link active">Login</a>
                       </li>
 
                   </ul>
